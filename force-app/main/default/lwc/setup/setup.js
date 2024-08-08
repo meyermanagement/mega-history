e23d59d2a0e40150322b94fb38348ca30fa255d4
Mark Meyer <meyermanagement@gmail.com>
2024-08-08 09:39:01 -0500

    complete connection check

--- force-app/main/default/lwc/setup/setup.js
+++ force-app/main/default/lwc/setup/setup.js
@@ -2,12 +2,22 @@ import { LightningElement, wire, track } from 'lwc';
 import { refreshApex } from "@salesforce/apex";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import checkAPIConnection from "@salesforce/apex/SetupController.checkMetadataAPIConnection";
+import getURL from "@salesforce/apex/SetupController.getOrgURL";
 
 export default class Setup extends LightningElement {
 
     @track showConnectionModal = false;
-    @track connectionFailure;
-    connectionClass = 'action-completed';
+    @track showRemoteSiteConfiguration = false;
+    wiredConnection;
+    orgURL;
+
+    get connectionFailure() {
+        return this.wiredConnection.data;
+    }
+
+    get connectionClass() {
+        return this.wiredConnection.data ? 'action-needed' : 'action-completed'
+    }
 
     connectedCallback(){
         //this.connectionFailure = true;
@@ -15,10 +25,24 @@ export default class Setup extends LightningElement {
 
     @wire(checkAPIConnection)
     getConnection(result) {
+        this.wiredConnection = result;
+        if (result.error) {
+            console.log(result.error.body.message);
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: "An error has occurred. Please contact the system administrator for further assistance.",
+                    message: result.error.body.message,
+                    variant: "error",
+                }),
+            );
+        }
+    }
+
+    @wire(getURL)
+    getOrgURL(result) {
         const{data, error} = result;
         if (data) {
-            this.connectionFailure = data;
-            if(this.connectionFailure) this.connectionClass = 'action-needed';
+            this.orgURL = data;
         } else if (error) {
             console.log(error.body.message);
             this.dispatchEvent(
@@ -31,15 +55,18 @@ export default class Setup extends LightningElement {
         }
     }
 
-    refresh(){
-        refreshApex(this.wiredDocuments);
-    }
 
     handleConnectionClick() {
-        this.showConnectionModal = true;
+        if(this.connectionFailure) this.showConnectionModal = true;
     }
 
     handleClose() {
+        this.showRemoteSiteConfiguration = false;
         this.showConnectionModal = false;
+        return refreshApex(this.wiredConnection);
+    }
+
+    addRSS() {
+        this.showRemoteSiteConfiguration = true;
     }
 }
