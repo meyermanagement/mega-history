9beff9ff6bcfee5983a1bc1cd16f87a67b82c20a
Mark Meyer <meyermanagement@gmail.com>
2024-08-25 20:02:21 -0500

    remove view setup

--- force-app/main/default/lwc/tracking/tracking.js
+++ force-app/main/default/lwc/tracking/tracking.js
@@ -1,33 +1,37 @@
 import { LightningElement, wire, track } from 'lwc';
 import { refreshApex } from "@salesforce/apex";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
-import getTrackingRecords from '@salesforce/apex/TrackingController.getRecords'
-import getObjectDetails from '@salesforce/apex/TrackingController.getObjectDetails'
-
+import { loadStyle } from 'lightning/platformResourceLoader';
+import iconColor from '@salesforce/resourceUrl/iconColor';
+import getTrackingRecords from '@salesforce/apex/TrackingController.getRecords';
+import getObjectDetails from '@salesforce/apex/TrackingController.getObjectDetails';
+import getObjectSelectedDetails from '@salesforce/apex/TrackingController.getObjectSelectedDetails';
+import getObjects from '@salesforce/apex/TrackingController.getObjects';
+import submitMetaData from '@salesforce/apex/TrackingController.submitMetaData';
 export default class Tracking extends LightningElement {
    
 
     @track columns = [
         {
             label: 'Object', 
-            initialWidth: 200,
+            initialWidth: 180,
             fieldName: 'objectName', 
             type: 'text'            
         },
         { 
             label: 'Parent Reference', 
-            initialWidth: 200,
+            initialWidth: 180,
             fieldName: 'parentRef', 
             type: 'text' 
         },
         { 
             label: 'Events', 
-            initialWidth: 250,
+            initialWidth: 200,
             fieldName: 'events', 
             type: 'text'
         },
         { 
-            label: 'Tracked Fields', 
+            label: 'Custom Tracked Fields', 
             fieldName: 'fields', 
             type: 'text'
         },
@@ -43,28 +47,57 @@ export default class Tracking extends LightningElement {
             }
         },
         {
-            label: 'Deployment Status', 
-            initialWidth: 180,
-            fieldName: 'statusLabel',
+            label: 'Metadata Status', 
+            initialWidth: 160,
+            fieldName: 'mdtStatusLabel',
             cellAttributes: { 
-                class: {fieldName: 'statusClass'}
+                class: {fieldName: 'mdtStatusClass'}
+            }
+        },
+        {
+            label: 'Trigger Status', 
+            initialWidth: 160,
+            fieldName: 'trigStatusLabel',
+            cellAttributes: { 
+                class: {fieldName: 'trigStatusClass'}
+            }
+        },
+        {
+            type: 'button-icon',
+            initialWidth: 60,
+            typeAttributes: { 
+                label: 'Edit', 
+                iconName: 'utility:delete',
+                name: 'delete_tracking', 
+                variant: 'border-filled',
+                title: 'Delete',
+                iconClass: 'myCustomDatatableIconColor'
             }
         }
     ];
 
     _wiredData;
     loading;
-    trackingData;
+    @track trackingData;
     @track editModal = false;
     showObjectLookup = false;
     @track selectedObject = {};
-    @track selectedFields = [];
+    @track options = [];
+    @track values = [];
+    @track requiredOptions = [];
+    @track parentRefs = [];
+    objects = [];
+    objectSelected = '';
+    @track deleteConfirmModal = false;
+    @track deployModal = false;
+    @track trackingDeployment;
+    
 
     get deploymentComplete(){
         var hasPending = false;
         if(this.trackingData){
             for(var obj of this.trackingData){
-                if(obj.statusLabel == 'Not Deployed') hasPending = true;
+                if(obj.trigStatusLabel != 'Deployed' || obj.mdtStatusLabel != 'Deployed') hasPending = true;
             }
         }
         return !hasPending;
@@ -78,8 +111,13 @@ export default class Tracking extends LightningElement {
         return this.selectedObject.trackDelete == true;
     }
 
+    get hasSelectedObject(){
+        return this.objectSelected != '' || JSON.stringify(this.selectedObject) != '{}';
+    }
+
     connectedCallback(){
         if(this.trackingData == undefined) this.loading = true;
+        loadStyle(this, iconColor);
     }
 
     @wire(getTrackingRecords)
@@ -101,23 +139,102 @@ export default class Tracking extends LightningElement {
     }
 
     newTracking() {
+        this.loading = true;
         this.showObjectLookup = true;
         this.editModal = true;
         this.selectedObject = {};
-        
-    }
-
-    editTracking(event) {
-        this.showObjectLookup = false;
-        this.editModal = true;
-        this.selectedObject = {...event.detail.row};
-        getObjectDetails({ wrapperString : JSON.stringify(this.selectedObject) })
+        getObjects()
         .then((data) => {
+            const items = [];
+            for(var o of data){
+                items.push({
+                    label: o.objectLabel+'('+o.objectName+')',
+                    value: o.objectName
+                });
+            }
+            this.objects.push(...items);
             this.loading = false;
-            this.selectedFields = data;
         })
         .catch(error => {
 			console.error(error);
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: "An error has occurred. Please contact the system administrator for further assistance.",
+                    message: error.body.message,
+                    variant: "error",
+                }),
+            );
+            this.loading = false;
+		}); 
+    }
+
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        console.log(actionName);
+        const row = event.detail.row;
+        console.log(row);
+        switch (actionName) {
+            case 'delete_tracking':
+                this.deleteTracking(row);
+                break;
+            case 'edit_tracking':
+                this.editTracking(row);
+                break;
+            default:
+        }
+    }
+
+    deleteTracking(row){
+        console.log(JSON.stringify(row));
+        this.deleteConfirmModal = true;
+        this.selectedObject = {...row};
+    }
+
+    editTracking(row) {
+        this.loading = true;
+        this.showObjectLookup = false;
+        this.editModal = true;
+        this.selectedObject = {...row};
+        getObjectDetails({ wrapperString : JSON.stringify(this.selectedObject) })
+        .then((data) => {
+            this.selectedObject = data;
+            const items = [];
+            const selected = [];
+            const required = [];
+            const parentItems = [];
+            for(var f of data.fieldList){
+                items.push({
+                    label: f.fieldLabel+'('+f.fieldAPIName+')',
+                    value: f.fieldAPIName
+                });
+                if(f.standardTracked){
+                    selected.push(f.fieldAPIName);
+                    required.push(f.fieldAPIName);
+                } else if(f.customTracked){
+                    selected.push(f.fieldAPIName);
+                }
+            }
+            for(var f in data.parentRefMap){
+                parentItems.push({
+                    label: data.parentRefMap[f]+'('+f+')',
+                    value: f
+                });
+            }
+            this.options.push(...items);
+            this.values.push(...selected);
+            this.requiredOptions.push(...required);
+            this.parentRefs.push(...parentItems);
+            this.loading = false;
+        })
+        .catch(error => {
+			console.error(error);
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: "An error has occurred. Please contact the system administrator for further assistance.",
+                    message: error.body.message,
+                    variant: "error",
+                }),
+            );
             this.loading = false;
 		}); 
         
@@ -125,16 +242,63 @@ export default class Tracking extends LightningElement {
 
     handleClose() {
         this.editModal = false;
-        return refreshApex(this._wiredData);
+        this.deleteConfirmModal = false;
+        refreshApex(this._wiredData);
+        this.options = [];
+        this.values = [];
+        this.requiredOptions = [];
+        this.parentRefs = [];
+        this.objectSelected = '';
     }
 
     handleSave(){
-        console.log('handleSave>>'+JSON.stringify(this.selectedObject));
         this.loading = true;
+        submitMetaData({ wrapperString : JSON.stringify(this.selectedObject), trackingData : JSON.stringify(this.trackingData), fields : this.values })
+        .then((data) => {
+            this.trackingData = data;
+        })
+        .catch(error => {
+			console.error(error);
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: "An error has occurred. Please contact the system administrator for further assistance.",
+                    message: error.body.message,
+                    variant: "error",
+                }),
+            );
+            this.loading = false;
+		}); 
         refreshApex(this._wiredData);
         this.selectedObject = {};
-        this.editModal = false;
+        this.handleClose();
         this.loading = false;
+    }
+
+    handleDelete(){
+        if(this.selectedObject.trigStatusLabel == 'Not Deployed' && this.selectedObject.mdtStatusLabel == 'Not Deployed') {
+            let trackingList = [];
+            for(var tracking of this.trackingData){
+                if(tracking.objectName != this.selectedObject.objectName) trackingList.push(tracking);
+            }
+            this.trackingData = trackingList;
+        } else {
+            let trackingList = [];
+            for(var tracking of this.trackingData){
+                if(tracking.objectName != this.selectedObject.objectName) trackingList.push(tracking);
+            }
+            this.selectedObject.mdtStatusLabel = 'Pending Removal';
+            this.selectedObject.mdtStatusClass = 'slds-text-color_weak slds-text-title_caps';
+            this.selectedObject.trigStatusLabel = 'Pending Removal';
+            this.selectedObject.trigStatusClass = 'slds-text-color_weak slds-text-title_caps';
+            trackingList.push(this.selectedObject);
+            this.trackingData = trackingList;
+        }
+        this.handleClose();
+    }
+
+    openDeployModal(){
+
+        this.deployModal = true;
     }
 
     deploy(){
@@ -143,32 +307,94 @@ export default class Tracking extends LightningElement {
         this.loading = false;
     }
 
-    handleObjectChange(event) {
-        this.selectedObject.objectName = event.detail.value;
+    handleObjectSelected(event) {
+        this.objectSelected = event.detail.value;
+        this.options = [];
+        this.values = [];
+        this.requiredOptions = [];
+        this.parentRefs = [];
+        getObjectSelectedDetails({ objectName : this.objectSelected })
+        .then((data) => {
+            this.selectedObject = {...data};
+            const items = [];
+            const selected = [];
+            const required = [];
+            const parentItems = [];
+            for(var f of data.fieldList){
+                items.push({
+                    label: f.fieldLabel+'('+f.fieldAPIName+')',
+                    value: f.fieldAPIName,
+                });
+                if(f.standardTracked){
+                    selected.push(f.fieldAPIName);
+                    required.push(f.fieldAPIName);
+                } else if(f.customTracked){
+                    selected.push(f.fieldAPIName);
+                }
+            }
+            for(var f in data.parentRefMap){
+                parentItems.push({
+                    label: data.parentRefMap[f]+'('+f+')',
+                    value: f
+                });
+            }
+            this.options.push(...items);
+            this.values.push(...selected);
+            this.requiredOptions.push(...required);
+            this.parentRefs.push(...parentItems);
+            this.loading = false;
+        })
+        .catch(error => {
+			console.error(error);
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: "An error has occurred. Please contact the system administrator for further assistance.",
+                    message: error.body.message,
+                    variant: "error",
+                }),
+            );
+            this.loading = false;
+		}); 
     }
 
     handleParentChange(event) {
-        this.selectedObject.parentRef = event.detail.value;
+        let tempObj = {...this.selectedObject};
+        tempObj.parentRef = event.detail.value;
+        this.selectedObject = {...tempObj};
     }
 
     handleTrackCreate(event) {
-        this.selectedObject.trackCreate = event.detail.checked;
+        let tempObj = {...this.selectedObject};
+        tempObj.trackCreate = event.detail.checked;
+        this.selectedObject = {...tempObj};
     }
 
     handleTrackDelete(event) {
-        this.selectedObject.trackDelete = event.detail.checked;
+        let tempObj = {...this.selectedObject};
+        tempObj.trackDelete = event.detail.checked;
+        this.selectedObject = {...tempObj};
     }
 
     handleTrackUndelete(event) {
-        this.selectedObject.trackUndelete = event.detail.checked;
+        let tempObj = {...this.selectedObject};
+        tempObj.trackUndelete = event.detail.checked;
+        this.selectedObject = {...tempObj};
     }
 
     handleAdditionalField1(event) {
-        this.selectedObject.additionalField1 = event.detail.value;
+        let tempObj = {...this.selectedObject};
+        tempObj.additionalField1 = event.detail.value;
+        this.selectedObject = {...tempObj};
     }
 
     handleAdditionalField2(event) {
-        this.selectedObject.additionalField2 = event.detail.value;
+        let tempObj = {...this.selectedObject};
+        tempObj.additionalField2 = event.detail.value;
+        this.selectedObject = {...tempObj};
+    }
+
+    handleFieldChange(event) {
+        this.values = event.detail.value;
     }
 
 }
